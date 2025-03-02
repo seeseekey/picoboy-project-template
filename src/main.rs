4760a167f819d36d8ba2fb25f074c3daf478df2a
seeseekey <info@seeseekey.net>
2025-03-02 09:33:08 +0100

    Build example.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
+// use embedded_hal::digital::OutputPin;
 use panic_probe as _;
 
 use rp2040_hal::Spi;
@@ -15,8 +15,9 @@ use rp2040_hal::Spi;
 use embedded_graphics::mono_font::ascii::FONT_6X10;
 use embedded_graphics::mono_font::MonoTextStyleBuilder;
 use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_graphics::text::{Baseline, Text};
 use embedded_graphics::prelude::*;
+use embedded_graphics::text::{Baseline, Text};
+use embedded_hal::digital::{InputPin, OutputPin};
 
 use sh1106::prelude::GraphicsMode;
 use sh1106::Builder;
@@ -31,11 +32,12 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+use cortex_m::asm::delay;
+use embedded_graphics::primitives::{Circle, PrimitiveStyleBuilder};
 use rp2040_hal::fugit::RateExtU32;
 
 #[entry]
 fn main() -> ! {
-
     info!("Program start");
 
     let mut pac = pac::Peripherals::take().unwrap();
@@ -73,7 +75,9 @@ fn main() -> ! {
     // Configure SPI pins
     let spi_sclk = pins.sck.into_function::<rp2040_hal::gpio::FunctionSpi>(); // SCK
     let spi_mosi = pins.mosi.into_function::<rp2040_hal::gpio::FunctionSpi>(); // MOSI
-    let spi_miso = pins.joystick_left.into_function::<rp2040_hal::gpio::FunctionSpi>(); // MISO (not needed)
+    let spi_miso = pins
+        .joystick_left
+        .into_function::<rp2040_hal::gpio::FunctionSpi>(); // MISO (not needed)
 
     // Create spi instance
     let spi = Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
@@ -82,7 +86,7 @@ fn main() -> ! {
     let spi = spi.init(
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
-        8_000_000u32.Hz(), // 8 MHz
+        8_000_000u32.Hz(),         // 8 MHz
         embedded_hal::spi::MODE_0, // MODE_0 for SH1106
     );
 
@@ -103,6 +107,7 @@ fn main() -> ! {
     display.init().unwrap();
     display.flush().unwrap();
 
+    // Create Hello world.
     let text_style = MonoTextStyleBuilder::new()
         .font(&FONT_6X10)
         .text_color(BinaryColor::On)
@@ -112,20 +117,69 @@ fn main() -> ! {
         .draw(&mut display)
         .unwrap();
 
-    Text::with_baseline("From Rust with love.", Point::new(0, 16), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+    Text::with_baseline(
+        "From Rust with love.",
+        Point::new(0, 16),
+        text_style,
+        Baseline::Top,
+    )
+    .draw(&mut display)
+    .unwrap();
 
     display.flush().unwrap();
+    delay.delay_ms(2500);
 
-    // Red led running indicator
+    // Ball
+    display.clear();
+
+    // Configuring joystick buttons
+    let mut joystick_up = pins.joystick_up.into_pull_up_input();
+    let mut joystick_down = pins.joystick_down.into_pull_up_input();
+    // let mut joystick_left = pins.joystick_left.into_pull_up_input();
+    let mut joystick_right = pins.joystick_right.into_pull_up_input();
+
+    let mut x: i32 = DISPLAY_WIDTH / 2;
+    let mut y: i32 = DISPLAY_HEIGHT / 2;
+
+    // Clear display
+    display.clear();
+
+    // Status led
     let mut led_pin = pins.led_red.into_push_pull_output();
+    led_pin.set_high().unwrap();
 
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        display.clear();
+
+        // Check entries and adjust position
+        if joystick_down.is_low().unwrap() {
+            y = y.saturating_add(2);
+        }
+
+        if joystick_up.is_low().unwrap() {
+            y = y.saturating_sub(2);
+        }
+
+        if joystick_right.is_low().unwrap() {
+            x = x.saturating_add(2);
+        }
+
+        // if joystick_left.is_low().unwrap() {
+        //     x = x.saturating_sub(2);
+        // }
+
+        // Draw a new circle
+        let new_circle = Circle::new(Point::new(x, y), 25).into_styled(
+            PrimitiveStyleBuilder::new()
+                .fill_color(BinaryColor::On)
+                .build(),
+        );
+
+        new_circle.draw(&mut display).unwrap();
+
+        display.flush().unwrap();
+
+        delay.delay_ms(50);
     }
 }
 
